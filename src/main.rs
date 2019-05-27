7ba86f5e2f86b91fccfb8566f5a504949c033d2c
Sambeau <531920324@qq.com>
2019-05-27 22:30:06 +0800

    use glium to display.

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+#[macro_use]
+extern crate glium;
+
+#[derive(Copy, Clone)]
+struct Vertex{
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn main() {
+    use glium::{glutin, Surface};
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let wb = glutin::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+
+    // vertex data
+    let vertex1 = Vertex{ position: [-0.5, -0.5]};
+    let vertex2 = Vertex{ position: [0.0, 0.5]};
+    let vertex3 = Vertex{ position: [0.5, -0.25]};
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    // shader
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main(){
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main(){
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    let mut closed = false;
+    while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+
+        events_loop.poll_events(|ev|{
+            match ev{
+                glutin::Event::WindowEvent{event, ..} => match event{
+                    glutin::WindowEvent::CloseRequested => closed = true,
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
+    }
+}
